93d09780e3dbe0e08ec7e244f9a93e8e63290d7c
Ryan Maulding <ryan@grox.org>
2015-06-20 17:01:32 -0700

    Dropped some items which were added over aggresively.

--- db/migrate/20150620151509_sorcery_remember_me.rb
+++
@@ -1,8 +0,0 @@
-class SorceryRememberMe < ActiveRecord::Migration
-  def change
-    add_column :people, :remember_me_token, :string, :default => nil
-    add_column :people, :remember_me_token_expires_at, :datetime, :default => nil
-
-    add_index :people, :remember_me_token
-  end
-end
--- db/migrate/20150620151511_sorcery_user_activation.rb
+++
@@ -1,9 +0,0 @@
-class SorceryUserActivation < ActiveRecord::Migration
-  def change
-    add_column :people, :activation_state, :string, :default => nil
-    add_column :people, :activation_token, :string, :default => nil
-    add_column :people, :activation_token_expires_at, :datetime, :default => nil
-
-    add_index :people, :activation_token
-  end
-end
